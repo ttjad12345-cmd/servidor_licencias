ab5fd7c7e600d1340ee0a03fc27a5a42a2ec89f6
ttjad12345-cmd <ttjad12345@gmail.com>
2026-08-28 18:31:16 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express'); 
+const app = express(); 
+app.use(express.json()); 
+const licenciasNube = { "PRO-2026-JUAN": { plan: "pro", areas: 6, activa: true, hardwareId: null }, "START-2026-ANA": { plan: "starter", areas: 2, activa: true, hardwareId: null } }; 
+app.listen(PORT, () = activo en puerto ${PORT}`)); 
